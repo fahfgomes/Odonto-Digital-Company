a8dfd5fb06fb200a208b4ce2bb95050a399e838f
Fabricio Gomes <54582091+fahfgomes@users.noreply.github.com>
2022-03-03 21:22:09 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'none')
+        e.style.display = 'block';
+    else
+        e.style.display = 'none';
+    }
